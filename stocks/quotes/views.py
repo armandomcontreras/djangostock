da56b88882bbb62180f26eab71e7ce6adf37e3f5
Armando Contreras <amcontreras10@gmail.com>
2024-03-25 23:05:02 -0600

    added api key and functions in home.html

--- stocks/quotes/views.py
+++ stocks/quotes/views.py
@@ -2,7 +2,18 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'home.html', {})
+    import requests
+    import json
+
+    # pk_9838a657f54d4ef0a27147ffd8012c1a
+    api_request = requests.get("https://cloud.iexapis.com/stable/stock/aapl/quote?token=pk_9838a657f54d4ef0a27147ffd8012c1a")
+
+    try:
+        api = json.loads(api_request.content)
+    except Exception as e:
+        api = "Error..."
+
+    return render(request, 'home.html', {'api': api})
 
 
 def about(request):
