519381479481bfc6deb088a2662963061ef59d23
Armando Contreras <amcontreras10@gmail.com>
2024-03-26 21:01:12 -0600

    add add_stock page

--- stocks/quotes/views.py
+++ stocks/quotes/views.py
@@ -7,14 +7,13 @@ def home(request):
 
     if request.method == 'POST':
         ticker = request.POST['ticker']
-        api_request = requests.get(
-            "https://cloud.iexapis.com/stable/stock/" + ticker + "/quote?token=pk_9838a657f54d4ef0a27147ffd8012c1a")
+        api_request = requests.get("https://cloud.iexapis.com/stable/stock/" + ticker + "/quote?token=pk_9838a657f54d4ef0a27147ffd8012c1a")
 
         try:
             api = json.loads(api_request.content)
         except Exception as e:
             api = "Error..."
-            return render(request, 'home.html', {'api': api})
+        return render(request, 'home.html', {'api': api})
 
     else:
         return render(request, 'home.html', {'ticker': "Enter a Ticker Symbol Above..."})
@@ -25,3 +24,7 @@ def home(request):
 
 def about(request):
     return render(request, 'about.html', {})
+
+def add_stock(request):
+    return render(request, 'add_stock.html', {})
+
