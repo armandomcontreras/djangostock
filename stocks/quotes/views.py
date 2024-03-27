a12bc746d7f0686df033c801d9094d99f0763900
Armando Contreras <amcontreras10@gmail.com>
2024-03-26 20:27:38 -0600

    stock market uploads

--- stocks/quotes/views.py
+++ stocks/quotes/views.py
@@ -5,15 +5,22 @@ def home(request):
     import requests
     import json
 
-    # pk_9838a657f54d4ef0a27147ffd8012c1a
-    api_request = requests.get("https://cloud.iexapis.com/stable/stock/aapl/quote?token=pk_9838a657f54d4ef0a27147ffd8012c1a")
+    if request.method == 'POST':
+        ticker = request.POST['ticker']
+        api_request = requests.get(
+            "https://cloud.iexapis.com/stable/stock/" + ticker + "/quote?token=pk_9838a657f54d4ef0a27147ffd8012c1a")
 
-    try:
-        api = json.loads(api_request.content)
-    except Exception as e:
-        api = "Error..."
+        try:
+            api = json.loads(api_request.content)
+        except Exception as e:
+            api = "Error..."
+            return render(request, 'home.html', {'api': api})
 
-    return render(request, 'home.html', {'api': api})
+    else:
+        return render(request, 'home.html', {'ticker': "Enter a Ticker Symbol Above..."})
+
+
+
 
 
 def about(request):
